d65dc15f0e93e89875bb8e808be3bd469f7b7319
jaeger2011 <t.jaeger2011@googlemail.com>
2014-05-27 08:49:10 +0200

    - Klasse FFT von GUI "bereinigt"
    - in Klasse Record einen Beispielbutton erzeugt. Dieser Button ist mit
    OnClickListener belegt, welcher einen Beispielton erzeugt.
    
    - Neue Klasse Tone erstellt. Diese Klasse wird beim Drücken des oben
    genannten Buttons aufgerufen.

--- PipeHype/src/com/pipehype/Record.java
+++ PipeHype/src/com/pipehype/Record.java
@@ -3,27 +3,45 @@ package com.pipehype;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
-public class Record extends ActionBarActivity {
+public class Record extends Activity {
+
+	 double BeispielTonFrequenz = 400;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		Button button_ton = (Button) findViewById(R.id.button1);
+		button_ton.setOnClickListener(new OnClickListener(){
+
+			
+			@Override
+			public void onClick(View v) {	 
+			//Ein Objekt der Klasse "Tone" wird mit der dem angegebenen Beispielton entsprechenden Frequenz erzeugt...
+			Tone tone = new Tone();
+			tone.genTone(BeispielTonFrequenz);
+			//...Der Ton wird abgespielt.
+			tone.playSound();
+			}});
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -31,34 +49,9 @@ public class Record extends ActionBarActivity {
 		getMenuInflater().inflate(R.menu.record, menu);
 		return true;
 	}
+	
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_record,
-					container, false);
-			return rootView;
-		}
-	}
 
 }
